f14a40f67451eaeb79e8f0c8c155a4d351499ef6
tetsuya28 <tet.lv11@gmail.com>
2022-03-10 14:30:29 +0900

    :tada:

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM golang:1.17 as builder
+
+ENV CGO_ENABLED=0
+ENV GOOS=linux
+ENV GOARCH=amd64
+WORKDIR /build
+
+COPY go.mod .
+COPY go.sum .
+RUN go mod download
+
+COPY . .
+
+RUN go build -o main main.go
+
+FROM alpine:3.15.0
+
+WORKDIR /
+
+COPY --from=builder /build/main /main
+
+USER nonroot
+
+CMD [ "/main" ]
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+
+	e.Get("/", listEnvs())
+	e.Get("/:name", listEnvs())
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
+}
+
+func listEnvs() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, os.Environ())
+	}
+}
+
+func getEnv() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, os.Getenv(c.Param("name")))
+	}
+}
